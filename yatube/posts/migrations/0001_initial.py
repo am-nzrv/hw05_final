e8b3640b97fe659cdcabb130af530d69adba6a26
Армен Назаров <amnazaroff@gmail.com>
2022-12-06 19:06:24 +0500

    Final

--- yatube/posts/migrations/0001_initial.py
+++ yatube/posts/migrations/0001_initial.py
@@ -47,8 +47,8 @@ class Migration(migrations.Migration):
             name='Follow',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('author', models.ForeignKey(on_delete='cascade', related_name='following', to=settings.AUTH_USER_MODEL)),
-                ('user', models.ForeignKey(on_delete='cascade', related_name='follower', to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following', to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='follower', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
