a1e95e56f4cc12a77838f607c981fa2f2ca7d0a6
Армен Назаров <amnazaroff@gmail.com>
2022-12-07 13:45:57 +0500

    Final

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h89@ps^kc6gp399xw_jr#=rs@zqzq6&(qwxk!jsa+x!waf_n5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
+    'amnazarov.ddns.net',
+    '51.250.107.160'
     'localhost',
     '127.0.0.1',
     '[::1]',
@@ -133,7 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
